763b851f6b3067fbdcd063aff70a181b62ea084d
samandargofurov <samandargofurov236@gmail.com>
2024-05-13 15:14:49 +0500

    added info

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,87 @@
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TodoInfo from "./components/TodoInfo";
+
+
+interface Task {
+  id?: number;
+  title?:string;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<any>([]); 
+  const [add, setAdd] = useState<{ title: string}>({title: ""});
+
+  useEffect(() => {
+    const oldTasks: Task[] | null = JSON.parse(localStorage.getItem('tasks') || '[]')
+    setTasks(oldTasks)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  },[])
+
+  function handleAdd(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault()
+    if(add?.title.trim() !== '') {
+      const taskItem: Task | null = {
+        id: tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1,
+        title: add?.title
+      }
+      setTasks([...tasks, taskItem])
+      setAdd({title: ''})
+    } else {
+      alert('please, enter task')
+    }
+  };
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault()
+    setAdd({...add, title: event.target.value})
+  }
+
+  function handleClick() {
+    if (document.getElementById("my_modal_1")) {
+      document.getElementById("my_modal_1").close();
+    }
+  }
+
 
   return (
     <>
-      <div>
+      <div className="container w-2/4 mx-auto mt-16">
+        <div className="flex items-center justify-between">
+          <button
+            className="btn btn-primary"
+            onClick={handleClick}
+          >
+            Add Task
+          </button>
+
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              <input type="text" value={add?.title} onChange={handleChange} className="outline-none border border-gray-200 w-full p-2 rounded-lg" placeholder="Enter task" />
+              <form method="dialog" className="modal-action flex gap-5">
+                <button onClick={handleAdd} className="btn">Add</button>
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </dialog>
+
+          <select className="select select-bordered w-28 max-w-sm">
+            <option selected value={"all"}>
+              All
+            </option>
+            <option value={"true"}>Active</option>
+            <option value={"false"}>Inactive</option>
+          </select>
+        </div>
         <div>
-          
+          <TodoInfo></TodoInfo>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TodoInfo/index.tsx
+++ src/components/TodoInfo/index.tsx
@@ -1,12 +1,57 @@
+import { FC } from "react";
+import { PiTrashSimpleFill } from "react-icons/pi";
+import { MdModeEditOutline } from "react-icons/md";
 
-function TodoInfo() {
+const TodoInfo:FC = () => {
+  const handleRemove = () => {};
+
+  const handleEdit = () => {};
+
   return (
     <>
-        <div>
-            <div></div>
-        </div>
-    </>
-  )
-}
+      <div className="container w-full mx-auto">
+        <div className="bg-gray-200 p-4 rounded-lg mt-10">
+          <div className="flex flex-col gap-5">
 
-export default TodoInfo
+            <div className="bg-white rounded-md p-3 flex justify-between">
+
+              <div className="flex items-center gap-2">
+                <div className="form-control">
+                  <label className="label cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className="checkbox checkbox-primary"
+                    />
+                  </label>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <h1 className="text-sm">Lorem ipsum dolor sit amet.</h1>
+                  <p className="text-xs">Lorem, ipsum.</p>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-5">
+                <span
+                  className="bg-gray-300 p-2 rounded-md text-lg cursor-pointer"
+                  onClick={handleRemove}
+                >
+                  <PiTrashSimpleFill></PiTrashSimpleFill>
+                </span>
+                <span
+                  className="bg-gray-300 p-2 rounded-md text-lg cursor-pointer"
+                  onClick={handleEdit}
+                >
+                  <MdModeEditOutline></MdModeEditOutline>
+                </span>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TodoInfo;
